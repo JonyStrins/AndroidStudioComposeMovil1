13f3a34dd133cb63843b30e00e6241f797558420
JonyStrins <slipknot1852@gmail.com>
2023-02-23 12:02:03 -0600

    Tip time avanzado llegamos hasta el paso 10

--- Dice Roller/app/src/main/java/com/jonystrins/diceroller/MainActivity.kt
+++ Dice Roller/app/src/main/java/com/jonystrins/diceroller/MainActivity.kt
@@ -3,13 +3,19 @@ package com.jonystrins.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jonystrins.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    DiceRollerApp()
                 }
             }
         }
@@ -30,14 +36,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier
+    .fillMaxSize()
+    .wrapContentSize(Alignment.Center)) {
+    var result by remember {
+        mutableStateOf(1)
+    }
+    val imageResource = when(result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = painterResource(id = imageResource), contentDescription = "${imageResource}")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { result = (1..6).random() }) {
+            Text(
+                stringResource(id = R.string.roll)
+            )
+        }
+    }
+}
+
+@Composable
+private fun getRandomDiceImage() : Int {
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun DiceRollerApp() {
     DiceRollerTheme {
-        Greeting("Android")
+        DiceWithButtonAndImage()
     }
 }
