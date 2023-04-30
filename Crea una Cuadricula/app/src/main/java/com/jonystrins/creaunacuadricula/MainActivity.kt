a925bbda23ba5288119f5a51bbdf5830e3fa8eda
JonyStrins <slipknot1852@gmail.com>
2023-04-30 15:37:09 -0600

    Cuadricula Terminada

--- Crea una Cuadricula/app/src/main/java/com/jonystrins/creaunacuadricula/MainActivity.kt
+++ Crea una Cuadricula/app/src/main/java/com/jonystrins/creaunacuadricula/MainActivity.kt
@@ -3,13 +3,22 @@ package com.jonystrins.creaunacuadricula
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jonystrins.creaunacuadricula.data.DataSource
+import com.jonystrins.creaunacuadricula.model.Topic
 import com.jonystrins.creaunacuadricula.ui.theme.CreaUnaCuadriculaTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +31,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    CuadriculaApp(
+                        modifier = Modifier.padding(8.dp)
+                    )
                 }
             }
         }
@@ -30,14 +41,76 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CuadriculaApp(modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier
+    ){
+        items(DataSource.topics){
+            topic -> Carta(topic = topic)
+        }
+    }
+}
+
+@Composable
+fun Carta(topic: Topic, modifier: Modifier = Modifier){
+    Card() {
+        Row{
+            Box{
+               Image(
+                   painter = painterResource(id = topic.imageRes),
+                   contentDescription = null,
+                   modifier = modifier
+                       .size(width = 68.dp, height = 68.dp)
+                       .aspectRatio(1f),
+                   contentScale = ContentScale.Crop
+               ) 
+            }
+            Column() {
+                Text(
+                    text = stringResource(id = topic.name),
+                    style = MaterialTheme.typography.subtitle1,
+                    modifier = Modifier.padding(
+                        start = 16.dp,
+                        top = 16.dp,
+                        end = 16.dp,
+                        bottom = 8.dp
+                    )
+                )
+                Row(verticalAlignment = Alignment.CenterVertically){
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_grain),
+                        contentDescription = null,
+                        modifier = Modifier.padding(
+                            start = 16.dp
+                        )
+                    )
+                    Text(
+                        text = topic.availableCourses.toString(),
+                        style = MaterialTheme.typography.subtitle2,
+                        modifier = Modifier.padding(
+                            start = 8.dp
+                        )
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    CreaUnaCuadriculaTheme {
-        Greeting("Android")
+    CreaUnaCuadriculaTheme() {
+        val topic = Topic(R.string.photography, 321, R.drawable.photography)
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Carta(topic = topic)
+        }
     }
 }
